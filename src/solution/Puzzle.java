59e1945051ba01bb68f3b849b2c3e9dffa3772a2
Kern <kding@csupomona.edu>
2014-04-22 16:15:10 -0700

    move the construct function position

--- src/solution/Puzzle.java
+++ src/solution/Puzzle.java
@@ -18,6 +18,34 @@ public class Puzzle {
     private Map<Integer, Node> explored;
     private List<Action> actions;
 
+    public Puzzle(int size, HeuristicFacotry.typeHeuristic hType, List<Integer> list) {
+        State curState;
+        if (list != null && list.size() > 0) {
+            List<List<Tile>> max = new ArrayList<List<Tile>>();
+            for (int i = 0; i < size; i++) {
+                List<Tile> row = new ArrayList<Tile>();
+                for (int j = 0; j < size; j++) {
+                    row.add(new Tile(list.get(i * size + j)));
+                }
+                max.add(row);
+            }
+            curState = new Plain(max);
+        } else {
+            curState = new Plain(size, false);
+        }
+
+        goalState = new Plain(size, true);
+        heuristicType = hType;
+        root = new Node(curState, goalState, 0, heuristicType);
+
+        frontier = new PriorityQueue<Node>();
+        explored = new HashMap<Integer, Node>();
+        actions = new ArrayList<Action>();
+        for (ActionFactory.typeAction aType : ActionFactory.typeAction.values()) {
+            actions.add(ActionFactory.CreateAction(aType));
+        }
+    }
+
     public Node getRoot() {
         return root;
     }
@@ -53,34 +81,6 @@ public class Puzzle {
     protected boolean AddFrontier(Node node) {
         //PrintNode("Frontier", node);
         return frontier.add(node);
-    }
-
-    public Puzzle(int size, HeuristicFacotry.typeHeuristic hType, List<Integer> list) {
-        State curState;
-        if (list != null && list.size() > 0) {
-            List<List<Tile>> max = new ArrayList<List<Tile>>();
-            for (int i = 0; i < size; i++) {
-                List<Tile> row = new ArrayList<Tile>();
-                for (int j = 0; j < size; j++) {
-                    row.add(new Tile(list.get(i * size + j)));
-                }
-                max.add(row);
-            }
-            curState = new Plain(max);
-        } else {
-            curState = new Plain(size, false);
-        }
-
-        goalState = new Plain(size, true);
-        heuristicType = hType;
-        root = new Node(curState, goalState, 0, heuristicType);
-
-        frontier = new PriorityQueue<Node>();
-        explored = new HashMap<Integer, Node>();
-        actions = new ArrayList<Action>();
-        for (ActionFactory.typeAction aType : ActionFactory.typeAction.values()) {
-            actions.add(ActionFactory.CreateAction(aType));
-        }
     }
 
     public Node Run() {
